94bec11f304f15d9d33574dd2b23a41eac463e21
Lee Ju Been <ljb7977@gmail.com>
2018-09-21 16:28:15 +0900

    rename files

--- 01_logistic_regression.py
+++ 01_logistic_regression.py
@@ -1,39 +1,35 @@
 import tensorflow as tf
 
 x_data = [
-    [0, 0],
-    [0, 1],
-    [1, 0],
-    [1, 1]
+    [1, 2, 3, 4, 5, 6],
+    [2, 3, 1, 3, 3, 2]
 ]
 
 y_data = [
-    [0],
-    [1],
-    [1],
-    [0]
+    [0, 0, 0, 1, 1, 1]
 ]
 
-X = tf.placeholder(tf.float32, shape=[None, 2])
-Y = tf.placeholder(tf.float32, shape=[None, 1])
+X = tf.placeholder(tf.float32, shape=[2, None])
+Y = tf.placeholder(tf.float32, shape=[1, None])
 W = tf.Variable(tf.random_normal([1, 2]))
 b = tf.Variable(tf.random_normal([1]))
 
-Y_ = tf.sigmoid(tf.matmul(W, tf.transpose(X))+b)
+Y_ = tf.sigmoid(tf.matmul(W, X)+b)
 
-print(Y_)
-print(Y)
-cost = -tf.reduce_mean(tf.transpose(Y)*tf.log(Y_)+(1-tf.transpose(Y))*tf.log(1-Y_))
+cost = -tf.reduce_mean(Y*tf.log(Y_)+(1-Y)*tf.log(1-Y_))
 train = tf.train.GradientDescentOptimizer(learning_rate=0.01).minimize(cost)
 
 predicted = tf.cast(Y_ > 0.5, dtype=tf.float32)
 accuracy = tf.reduce_mean(tf.cast(tf.equal(predicted, Y), dtype=tf.float32))
 
+print(predicted)
+print(accuracy)
+
 with tf.Session() as sess:
     init = tf.global_variables_initializer()
     sess.run(init)
 
-    for step in range(100000):
+    for step in range(50000):
         cost_val, _ = sess.run([cost, train], feed_dict={X: x_data, Y: y_data})
         if step % 200 == 0:
             print(step, cost_val)
